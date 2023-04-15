af0ddc39ad7cb070ebac456b7a024a4510b8947c
Adam-Abdulhalim <adamabdul5211@gmail.com>
2023-04-15 18:55:06 -0400

    Group Saving/Deleting, Hover text, Opening tabs by group and separately
    Tweaks on saving/deleting functionality, added hover text to display URL's of tabs, finalized functionality for opening all URLs in a group as well as opening them separately when clicked.

--- TabUltra/popup.js
+++ TabUltra/popup.js
@@ -1,5 +1,5 @@
-//Function that takes a group of tabs as input and saves them
-//into chrome.storage
+//Function that takes a group of tabs and a name as input and saves them
+//into chrome.storage.local
 function saveGroup(saved_tabs, gName){
 
     //Save group under given key
@@ -13,6 +13,7 @@ function saveGroup(saved_tabs, gName){
     });
 };
 
+/* NO LONGER NECESSARY, KEEPING IT JUST IN CASE, I GUESS
 //Function that displays the group of tabs currently saved
 function displayGroups(){
 
@@ -39,43 +40,87 @@ function displayGroups(){
         });
     });
 }
-
+*/
+/*This function retrieves all the saved groups, displays them as <p>
+elements, creates buttons for each that can delete the group or open
+all tabs in the group, and allows you to click on a single tab to open it
+*/
 function displayAll(){
 
+    //Retrieve all groups from storage
     chrome.storage.local.get(null, function(items) {
+        //This is where we are outputting the groups on our popup
         const container = this.document.querySelector("#group_container");
+
+        //Iterate through each key-value group pair (groupName-group pair)
         for (let key in items) {
             console.log("                 " + key);
 
+            //Create title for group
             var title = document.createElement("h3");
             var titleText = document.createTextNode(key);
             title.appendChild(titleText);
 
+            //Create button to open all tabs in group
+            var openButton = document.createElement("button");
+            openButton.textContent = "Open all tabs in Group";
+
+            //Store all tabs for specific group
+            const groupUrls = [];
+            items[key].forEach((tab) => {
+                groupUrls.push(tab.url);
+            });
+
+            //Use stored tabs to open all when button is clicked
+            openButton.addEventListener("click", function(){
+                groupUrls.forEach((url) => {
+                    chrome.tabs.create({url: url});
+                });
+            });
+
+            //Create button to delete group
             var delButton = document.createElement("button");
-            delButton.textContent = "Delete Group"
+            delButton.textContent = "Delete Group";
+
+            //Use storage remove function to delete group when clicked
             delButton.addEventListener("click", function(){
                 chrome.storage.local.remove(key);
-                //alert(key + " WOULD BE DELETED");
+                alert("GROUP '" + key + "' HAS BEEN DELETED");
                 location.reload();
-            })
+            });
 
+            //Create list for tabs in group
             var groupList = document.createElement("ul");
 
+            //Iterate through each tab in said group
             items[key].forEach((tab) => {
                 console.log(tab.title);
 
+                //Create list item and text for current tab
                 const tabElement = document.createElement("li");
                 const p = document.createElement("p");
+                p.title = tab.url;
                 p.innerText = tab.title;
+
+                //Functionality for opening a tab when clicked
                 p.addEventListener("click", () => {
                     chrome.tabs.create({url: tab.url});
                 })
+                //Add paragraph text to list item
                 tabElement.appendChild(p);
+
+                //Add list item to list of tabs
                 groupList.appendChild(tabElement);
             });
+
+            //In order, add title of group, open button, close button,
+            //and list of tabs to the container
             container.appendChild(title);
+            container.appendChild(openButton);
             container.appendChild(delButton);
             container.appendChild(groupList);
+
+            //This process is repeated for each group of tabs
         }
     });
 }
@@ -99,6 +144,7 @@ window.addEventListener("DOMContentLoaded", function(){
 
             //Make the text equal to the tab's title
             p.innerText = tab.title;
+            p.title = tab.url;
 
             //Create button for closing tab
             const button = document.createElement("button");
@@ -136,7 +182,6 @@ window.addEventListener("DOMContentLoaded", function(){
     });
 
     //This is to display all saved/grouped tabs upon startup/load
-    //displayGroups();
     displayAll();
 
 
@@ -147,19 +192,25 @@ window.addEventListener("DOMContentLoaded", function(){
 const saveButton = document.getElementById("save");
 saveButton.addEventListener("click", function() {
     
+    //Retrieve user input for group name
     var groupName = document.getElementById("name").value;
 
+    //Check to make sure user has inputted group name
     if(groupName === ""){
         alert("ERROR: MUST GIVE GROUP A NAME");
         return;
     }
 
+    //Capitalize first letter of group name in order for it to output
+    //in alphabetical order
     groupName = groupName.charAt(0).toUpperCase() + groupName.slice(1);
 
     console.log(groupName);
 
+    //Reset user input textbox
     document.getElementById("name").value = "";
 
+    //Retrieve tabs and store them using saveGroup()
     chrome.runtime.sendMessage({ type: "getTabs"}, function (response){
         saveGroup(response, groupName);
     });
